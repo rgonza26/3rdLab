9f1f91cf3a3c50a0fe98c4a6df6cdb9f9fa4b83f
ptjohns2 <ptjohns2@asu.edu>
2014-03-18 12:36:29 -0700

    Fix main loop sentinel value

--- main.c
+++ main.c
@@ -29,8 +29,8 @@ int main(int argc, const char * argv[])
         add_token_to_list(token_list, token);
         print_token(token);
     }
-    while (strncmp(token->token_val.str, ".", 1) != 0);	/*What is the sentinal value that ends this loop? "." */
-    
+    while (strncmp(token->literalValue.valString, ".", 1) != 0);	/*What is the sentinal value that ends this loop? "." */
+
     quit_scanner(source_file, token_list);
     return 0;
 }
